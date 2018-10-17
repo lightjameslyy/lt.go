41978dc5818b98176cf33c10cfeb79f949e1cb3a
lightjameslyy <lightjameslyy@gmail.com>
2018-10-18 00:00:05 +0800

    leet_0862: on debugging

--- leetcode/leet_0862/leet_0862.go
+++ leetcode/leet_0862/leet_0862.go
@@ -12,13 +12,61 @@ func ShortestSubarray(A []int, K int) int {
 		curlen := 50001 // A.length <= 50000
 		var l, r int
 		lenA := len(A)
-		for r < lenA {
+		for r < lenA && l < lenA {
+			curnum := A[r]
+			if r-l == curlen {
+				if curnum <= 0 {
+					l = r + 1
+					cursum = 0
+				} else if curnum <= A[l] {
+					cursum -= A[l]
+					cursum -= A[l+1]
+					l += 2
+					for A[l] <= 0 {
+						l++
+					}
+				} else {
+					for curnum >= A[l] {
+						curnum -= A[l]
+						l++
+					}
+					if r-l+1 < curlen {
+						curlen = r - l + 1
+						if curlen == 2 {
+							return 2
+						}
+					} else {
+						cursum -= A[l]
+						cursum -= A[l+1]
+						l += 2
+						for A[l] <= 0 {
+							l++
+						}
+					}
+				}
+				continue
+			}
 			cursum += A[r]
 			if cursum <= 0 {
 				l = r + 1
+				cursum = 0
 			} else if cursum >= K {
+				cursum = A[r]
+				l = r
+				for cursum < K {
+					l--
+					cursum += A[l]
+				}
 				if r-l+1 < curlen {
 					curlen = r - l + 1
+				}
+				if curlen == 2 {
+					return 2
+				}
+			} else {
+				if r-l+1 >= curlen {
+					cursum -= A[l]
+					l++
 				}
 			}
 			r++
