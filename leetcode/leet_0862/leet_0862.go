89aa0b5b6f313b855bd935a022d62867cd61fe7e
lightjames <lightjameslyy@gmail.com>
2018-10-18 13:23:54 +0800

    leet_0862: on debugging

--- leetcode/leet_0862/leet_0862.go
+++ leetcode/leet_0862/leet_0862.go
@@ -1,6 +1,7 @@
 package leet_0862
 
-func ShortestSubarray(A []int, K int) int {
+// time limit exceeded
+func ShortestSubarray1(A []int, K int) int {
 	res := -1
 	for _, val := range A {
 		if val >= K {
@@ -12,61 +13,47 @@ func ShortestSubarray(A []int, K int) int {
 		curlen := 50001 // A.length <= 50000
 		var l, r int
 		lenA := len(A)
-		for r < lenA && l < lenA {
-			curnum := A[r]
-			if r-l == curlen {
-				if curnum <= 0 {
-					l = r + 1
-					cursum = 0
-				} else if curnum <= A[l] {
-					cursum -= A[l]
-					cursum -= A[l+1]
-					l += 2
-					for A[l] <= 0 {
-						l++
-					}
-				} else {
-					for curnum >= A[l] {
-						curnum -= A[l]
-						l++
-					}
-					if r-l+1 < curlen {
-						curlen = r - l + 1
-						if curlen == 2 {
-							return 2
-						}
-					} else {
-						cursum -= A[l]
-						cursum -= A[l+1]
-						l += 2
-						for A[l] <= 0 {
-							l++
-						}
-					}
-				}
-				continue
-			}
+		for r < lenA {
 			cursum += A[r]
 			if cursum <= 0 {
 				l = r + 1
 				cursum = 0
-			} else if cursum >= K {
-				cursum = A[r]
+				r++
+				continue
+			}
+			if cursum >= K {
 				l = r
-				for cursum < K {
+				tmpsum := 0
+				for tmpsum < K {
+					tmpsum += A[l]
 					l--
-					cursum += A[l]
 				}
+				l++
 				if r-l+1 < curlen {
 					curlen = r - l + 1
+					if curlen == 2 {
+						return 2
+					}
 				}
-				if curlen == 2 {
-					return 2
+			}
+			// find sub max sum as next candidate sequence
+			if r-l+1 >= curlen {
+				submax := 0
+				subsum := 0
+				p := r
+				index := l
+				for p > l {
+					subsum += A[p]
+					if subsum > submax {
+						submax = subsum
+						index = p
+					}
+					p--
 				}
-			} else {
-				if r-l+1 >= curlen {
-					cursum -= A[l]
-					l++
+				cursum = submax
+				l = index
+				if cursum >= K && r-l+1 < curlen {
+					curlen = r - l + 1
 				}
 			}
 			r++
@@ -74,6 +61,59 @@ func ShortestSubarray(A []int, K int) int {
 		if curlen < 50001 {
 			res = curlen
 		}
+	}
+	return res
+}
+
+// wrong
+func ShortestSubarray2(A []int, K int) int {
+	lenA := len(A)
+	min_len := lenA + 1
+	cursum, start := 0, 0
+	for i := 0; i < lenA; i++ {
+		for cursum < K && i < lenA {
+			cursum += A[i]
+			i++
+		}
+		for cursum >= K && start < lenA {
+			if i-start < min_len {
+				min_len = i - start
+			}
+			cursum -= A[i]
+			i++
+		}
+	}
+	return min_len
+}
+
+func ShortestSubarray(A []int, K int) int {
+	for _, val := range A {
+		if val >= K {
+			return 1
+		}
+	}
+	dp := make([]int, len(A)+1)
+	for i := range A {
+		dp[i+1] = dp[i] + A[i]
+	}
+
+	res := len(dp)
+	Q := []int{0}
+	for i := 1; i < len(dp); i++ {
+		for len(Q) > 0 && dp[i]-dp[Q[0]] >= K {
+			if i-Q[0] < res {
+				res = i - Q[0]
+				Q = Q[1:]
+			}
+		}
+		for len(Q) > 0 && dp[i] < dp[Q[len(Q)-1]] {
+			Q = Q[:len(Q)-1]
+		}
+		Q = append(Q, i)
+	}
+
+	if res == len(dp) {
+		return -1
 	}
 	return res
 }
