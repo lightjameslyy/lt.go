a5791c1c586f7ee1f7b4fe8380735db5be183b7f
lightjames <lightjameslyy@gmail.com>
2018-10-18 16:37:23 +0800

    gobyexample: values

---
+++ gobyexample/values/values.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang") // Strings can be added together with +
+
+	fmt.Println("1+1 = ", 1+1)
+	fmt.Println("7.0/3.0 = ", 7.0/3.0)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
