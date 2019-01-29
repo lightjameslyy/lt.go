03a625fc6624ec515770ba946dec0f1611b89089
lightjameslyy <lightjameslyy@gmail.com>
2019-01-29 14:59:23 +0800

    leetcode 118: AC

---
+++ leetcode/array/basics/118.pascals-triangle.go
@@ -0,0 +1,51 @@
+/*
+ * @lc app=leetcode id=118 lang=golang
+ *
+ * [118] Pascal's Triangle
+ *
+ * https://leetcode.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (43.92%)
+ * Total Accepted:    219.2K
+ * Total Submissions: 499.2K
+ * Testcase Example:  '5'
+ *
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's
+ * triangle.
+ *
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ *
+ * Example:
+ *
+ *
+ * Input: 5
+ * Output:
+ * [
+ * ⁠    [1],
+ * ⁠   [1,1],
+ * ⁠  [1,2,1],
+ * ⁠ [1,3,3,1],
+ * ⁠[1,4,6,4,1]
+ * ]
+ *
+ *
+ */
+// package main
+
+func generate(numRows int) [][]int {
+	res := make([][]int, numRows)
+	for i := 0; i < numRows; i++ {
+		res[i] = make([]int, i+1)
+		for j := 0; j < i+1; j++ {
+			if j == 0 || j == i {
+				res[i][j] = 1
+			} else {
+				res[i][j] = res[i-1][j-1] + res[i-1][j]
+			}
+		}
+	}
+	return res
+}
