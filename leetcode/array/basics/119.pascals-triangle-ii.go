c9c599111b7d95a1daefb37a4977b1efd3b8e40c
lightjameslyy <lightjameslyy@gmail.com>
2019-01-29 15:36:43 +0800

    leetcode 119: AC

---
+++ leetcode/array/basics/119.pascals-triangle-ii.go
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=119 lang=golang
+ *
+ * [119] Pascal's Triangle II
+ *
+ * https://leetcode.com/problems/pascals-triangle-ii/description/
+ *
+ * algorithms
+ * Easy (41.46%)
+ * Total Accepted:    178.5K
+ * Total Submissions: 430.6K
+ * Testcase Example:  '3'
+ *
+ * Given a non-negative index k where k ≤ 33, return the kth index row of the
+ * Pascal's triangle.
+ *
+ * Note that the row index starts from 0.
+ *
+ *
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ *
+ * Example:
+ *
+ *
+ * Input: 3
+ * Output: [1,3,3,1]
+ *
+ *
+ * Follow up:
+ *
+ * Could you optimize your algorithm to use only O(k) extra space?
+ *
+ */
+// package main
+
+func getRow(rowIndex int) []int {
+	res := make([]int, rowIndex+1)
+	res[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		res[i] = 1
+		// update res backwards to use only O(k) extra space
+		for j := i - 1; j >= 1; j-- {
+			res[j] = res[j] + res[j-1]
+		}
+	}
+	return res
+}
